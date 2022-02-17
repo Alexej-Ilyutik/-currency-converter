caa2c75ee652637ec6c7daaa3e0783d95a855b0a
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-02-17 23:39:57 +0300

    feat: add change currency with date

--- converter/script.js
+++ converter/script.js
@@ -3,6 +3,7 @@ const eur = 2.91;
 const cny = 0.4;
 const rub = 0.03;
 
+
 const DATE = document.getElementById('date_id');
 const MAIN_SELECT = document.querySelector('.main-select');
 const AMOUNT_VALUE = document.getElementById('amount');
@@ -13,74 +14,90 @@ const CNY_VALUE = document.getElementById('CNY');
 const RUB_VALUE = document.getElementById('RUB');
 const BOX = document.querySelectorAll('.box');
 let lastIndex = 0;
+let coefficient;
+let rate;
 
 DATE.valueAsDate = new Date();
 
 DATE.addEventListener('change', changeDate);
 
+
 function changeDate() {
+  const randomRate = Math.random() * (1.1 - 0.9) + 0.9;
   const date = new Date().getDate();
   const currentDate = DATE.valueAsDate.getDate();
   if (currentDate > date) {
     alert("Error! Choose today's or past date!");
     DATE.valueAsDate = new Date();
+  }else if (currentDate < date) {
+    coefficient = +randomRate.toFixed(2);
+   changeCurrencyValue();
+  }else{
+    coefficient = 1;
+    changeCurrencyValue();
   }
 }
 
-MAIN_SELECT.addEventListener('change', changeCurrencyValue);
+changeDate();
 
-AMOUNT_VALUE.addEventListener('change', changeCurrencyValue);
 
-changeCurrencyValue();
+MAIN_SELECT.addEventListener('change', changeDate);
 
-function changeCurrencyValue() {
+AMOUNT_VALUE.addEventListener('change', changeDate);
+
+function hideBox(){
   BOX[lastIndex].style.display = 'flex';
   let index = MAIN_SELECT.selectedIndex;
   BOX[index].style.display = 'none';
   lastIndex = index;
+}
+
+
+function changeCurrencyValue() {
+  hideBox();
   if (MAIN_SELECT.value === 'BYN') {
-    changeBYNValue();
+    changeBYNValue(coefficient);
   } else if (MAIN_SELECT.value === 'USD') {
-    changeUSDValue();
+    changeUSDValue(coefficient);
   } else if (MAIN_SELECT.value === 'EUR') {
-    changeEURValue();
+    changeEURValue(coefficient);
   } else if (MAIN_SELECT.value === 'CNY') {
-    changeCNYValue();
+    changeCNYValue(coefficient);
   } else if (MAIN_SELECT.value === 'RUB') {
-    changeRUBValue();
+    changeRUBValue(coefficient);
   }
 }
 
-function changeBYNValue() {
-  USD_VALUE.value = (AMOUNT_VALUE.value * usd).toFixed(2);
-  EUR_VALUE.value = (AMOUNT_VALUE.value * eur).toFixed(2);
-  CNY_VALUE.value = (AMOUNT_VALUE.value * cny).toFixed(2);
-  RUB_VALUE.value = (AMOUNT_VALUE.value * rub).toFixed(2);
+function changeBYNValue(k) {
+  USD_VALUE.value = (AMOUNT_VALUE.value / usd*k).toFixed(2);
+  EUR_VALUE.value = (AMOUNT_VALUE.value / eur*k).toFixed(2);
+  CNY_VALUE.value = (AMOUNT_VALUE.value / cny*k).toFixed(2);
+  RUB_VALUE.value = (AMOUNT_VALUE.value / rub*k).toFixed(2);
 }
 
-function changeUSDValue() {
-  BYN_VALUE.value = (AMOUNT_VALUE.value * usd).toFixed(2);
+function changeUSDValue(k) {
+  BYN_VALUE.value = (AMOUNT_VALUE.value * usd * k).toFixed(2);
   EUR_VALUE.value = (BYN_VALUE.value / eur).toFixed(2);
   CNY_VALUE.value = (BYN_VALUE.value / cny).toFixed(2);
   RUB_VALUE.value = (BYN_VALUE.value / rub).toFixed(2);
 }
 
-function changeEURValue() {
-  BYN_VALUE.value = (AMOUNT_VALUE.value * eur).toFixed(2);
+function changeEURValue(k) {
+  BYN_VALUE.value = (AMOUNT_VALUE.value * eur * k).toFixed(2);
   USD_VALUE.value = (BYN_VALUE.value / usd).toFixed(2);
   CNY_VALUE.value = (BYN_VALUE.value / cny).toFixed(2);
   RUB_VALUE.value = (BYN_VALUE.value / rub).toFixed(2);
 }
 
-function changeCNYValue() {
-  BYN_VALUE.value = (AMOUNT_VALUE.value * cny).toFixed(2);
+function changeCNYValue(k) {
+  BYN_VALUE.value = (AMOUNT_VALUE.value * cny * k).toFixed(2);
   USD_VALUE.value = (BYN_VALUE.value / usd).toFixed(2);
   EUR_VALUE.value = (BYN_VALUE.value / eur).toFixed(2);
   RUB_VALUE.value = (BYN_VALUE.value / rub).toFixed(2);
 }
 
-function changeRUBValue() {
-  BYN_VALUE.value = (AMOUNT_VALUE.value * rub).toFixed(2);
+function changeRUBValue(k) {
+  BYN_VALUE.value = (AMOUNT_VALUE.value * rub * k).toFixed(2);
   USD_VALUE.value = (BYN_VALUE.value / usd).toFixed(2);
   EUR_VALUE.value = (BYN_VALUE.value / eur).toFixed(2);
   CNY_VALUE.value = (BYN_VALUE.value / cny).toFixed(2);
